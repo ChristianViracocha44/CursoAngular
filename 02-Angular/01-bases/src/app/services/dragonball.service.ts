4b05b679b735c9cd4b41131404625de188656994
ChristianViracocha <angelcielo99@gmail.com>
2025-05-02 01:18:17 -0500

    Uso del Local Storage

--- 02-Angular/01-bases/src/app/services/dragonball.service.ts
+++ 02-Angular/01-bases/src/app/services/dragonball.service.ts
@@ -1,14 +1,28 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+
+const loadFromLocalStorage = (): Character[] => {
+  const characters = localStorage.getItem('characters');
+  return characters ? JSON.parse(characters) : [];
+};
+
 
 @Injectable({ providedIn: 'root' })
 export class DragonBalllService {
   constructor() {}
 
-  charecters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 8000 },
-  ]);
+  charecters= signal<Character[]>(loadFromLocalStorage());
+
+  //charecters = signal<Character[]>([
+  //  { id: 1, name: 'Goku', power: 9001 },
+  //  { id: 2, name: 'Vegeta', power: 8000 },
+  //]);
+
+saveToLocalStorage = effect(() => {
+    //console.log(`Guardando en localStorage ${this.charecters().length}`);
+    localStorage.setItem('characters', JSON.stringify(this.charecters()));
+  });
+
   addCharacter(newCharacter: Character) {
     // Validar que el nombre y el poder no sean vacíos o nulos
     this.charecters.update((list) => [...list, newCharacter]);
