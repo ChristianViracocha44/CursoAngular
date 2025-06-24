43919f0e9453ab84e6010f11910c3c781ad6053b
ChristianViracocha <angelcielo99@gmail.com>
2025-06-24 01:41:39 -0500

    Implementacion del Scroll Infinito
    implementacion del servicio para guardar el posicionamiento del scroll

--- 02-Angular/02-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
+++ 02-Angular/02-gifs-app/src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject, signal } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, signal, viewChild } from '@angular/core';
 import { GifListComponent } from "../../components/gif-list/gif-list.component";
 import { GifsService } from '../../services/gifs.service';
+import { ScrollStateService } from 'src/app/shared/services/scroll-state.service';
 
 /*
 const imageUrls: string[] = [
@@ -20,12 +21,40 @@ const imageUrls: string[] = [
 */
 @Component({
   selector: 'app-trending-page',
-  imports: [GifListComponent],
+  imports: [],
   templateUrl: './trending-page.component.html',
   styleUrl: './trending-page.component.css'
 })
-export default class TrendingPageComponent {
+export default class TrendingPageComponent implements AfterViewInit {
   //gifs= signal(imageUrls);
   gifService = inject(GifsService);
+  scrollStateService = inject(ScrollStateService)
+
+  scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv');
+
+  ngAfterViewInit(): void {
+      const scrollDiv = this.scrollDivRef()?.nativeElement;
+      if (!scrollDiv) return;
+      scrollDiv.scrollTop = this.scrollStateService.trendingScrollState();
+  }
+
+  onScroll(event : Event) {
+    //console.log('Scroll event:', event);
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    //console.log(scrollDiv);
+    if (!scrollDiv) return;
+
+    const scrollTop = scrollDiv.scrollTop;
+    const scrollHeight = scrollDiv.scrollHeight;
+    const clientHeight = scrollDiv.clientHeight;
+
+    const isAtBottom = scrollTop + clientHeight +300>= scrollHeight; // 300px tolerance
+    this.scrollStateService.trendingScrollState.set(scrollTop);
+    //console.log({isAtBottom, scrollTop, clientHeight, scrollHeight});
+    if (isAtBottom) {
+      //console.log('Reached the bottom of the scroll div');
+      this.gifService.loadTrendingGifs();
+    }
+  }
 
 }
